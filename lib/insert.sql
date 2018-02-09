cc2be2de683124ae76340dad59691ac82b3791e6
Petya Djanovska <petya.djanovska@gmail.com>
2018-02-09 19:05:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "brown", "playful", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "grey", "grumpy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "brown", "kind", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NILL, 3, "M", "brown", "furious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "M", "brown", "furious", 1);
